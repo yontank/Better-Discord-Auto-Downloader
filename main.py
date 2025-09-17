149ca7d3e90c68c5aedc372338f7642a95efdcd7
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-09-17 21:23:29 +0300

    created main function

--- main.py
+++ main.py
@@ -1,64 +1,31 @@
-import os
-import re
-import requests
-import tempfile
-
-
-def get_discord_path() -> tuple[str | bool]:
-    discord_path = os.path.join(os.getenv('LOCALAPPDATA'), 'Discord')
-    # app-1.0.9208','modules', 'discord_desktop_core-1', 'discord_desktop_core', 'index.js
-
-    if not os.path.exists(discord_path):
-        return ("", False)
-
-    regex_find_app_directory = re.compile(r'app-\d+\.\d+(\.\d+)?')
-
-    dirs = os.listdir(discord_path)
-
-    app_folder = list(filter(regex_find_app_directory.match, dirs))
-
-    if len(app_folder) != 1:
-        return ("", False)
-
-    app_folder = app_folder[0]
-
-    discord_path = os.path.join(discord_path, app_folder, 'modules',
-                                'discord_desktop_core-1', 'discord_desktop_core', 'index.js')
-
-    return (discord_path, True)
-
-
-def get_bc_location(file: str) -> tuple[str, bool]:
-    rx_get_better_discord_path = re.compile(r"^require\(\"(.*)\"\)\;$")
-
-    with open(file, 'r') as f:
-        for line in f:
-            if ('betterdiscord.asar' in line) or ('BetterDiscord' in line):
-                return re.match(rx_get_better_discord_path, line).group(1)
-
-
-def download_bc():
-    tmp = os.path(tempfile.gettempdir())
-
-    if not os.path.exists(tmp):
-        print('Err, Couldn\' set a temporary directory')
-
-    # response = requests.get(download_url, allow_redirects=True)
-    # print(tmp)
-    # if response.status_code == 200:
-    #     with open(tmp, 'wb') as output:
-    #         output.write(response.content)
+from venv import create
+from installer import *
+from paths import *
 
 
 def main():
     discord_path = get_discord_path()
-    print(discord_path)
-    bc_path = get_bc_location(discord_path[0])
+    appdata_path = os.getenv("APPDATA")
+    if appdata_path == None:
+        raise Exception("Not Running In Windows")
 
-    print(bc_path)
+    if not discord_path:
+        raise Exception("Didn't Find discord path.")
 
-    if (os.path.exists(bc_path)):
-        print("Discord is connected to better discord!")
+    if not (os.path.join(os.getenv('APPDATA', 'BetterDiscord'))):
+        if create_bc_path() == None:
+            raise Exception("Couldn't Install Better Discord ")
+
+    # Get ASAR File location from discord, if set.
+    bc_asar_path = get_bc_asar_location(discord_path)
+
+    if bc_asar_path == None:
+        # Inject the file with Better Discords code, by requiring it to run.
+        with open(discord_path, 'w') as injection_file:
+            injection_file.write(
+                f'\nrequire({os.path.join(appdata_path, "BetterDiscord", "data", "betterdiscord.asar")});')
+
+    return
 
 
 if __name__ == '__main__':
