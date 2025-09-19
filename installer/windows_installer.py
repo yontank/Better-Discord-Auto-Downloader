8c528646ec643c9b478365ed076ef5279e9f1216
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-09-19 17:21:37 +0300

    implement windows installer stuff

--- installer/windows_installer.py
+++ installer/windows_installer.py
@@ -1,14 +1,16 @@
 from typing import override
 from abstract import abstract_installer
-
+import psutil
 import os
 from turtle import down
 import requests
 
 
-class Windows(abstract_installer.AbstractInstaller):
+from paths.windows_paths import WindowsPaths
 
-    @override
+
+class WindowsInstaller(abstract_installer.AbstractInstaller):
+
     def install_bc(self, path: str):
         appdata = os.getenv('APPDATA')
 
@@ -31,3 +33,23 @@ class Windows(abstract_installer.AbstractInstaller):
 
     def install_discord(self) -> str | None:
         raise NotImplementedError()
+
+    def execute(self):
+        paths = WindowsPaths()
+        proc_name = 'discord.exe'
+
+        discord_inject_file = paths.get_discord_path()
+
+        if (discord_inject_file == None):
+            raise Exception('Cannot find discord path')
+
+        for proc in psutil.process_iter():
+            if proc.name().lower() == proc_name:
+                proc.kill()
+        # Todo: Write into Injection file, adding the asar file from Better Discord.
+        try:
+            with open(discord_inject_file, 'w') as OF:
+                OF.writelines(
+                    ["module.exports = require('./core.asar');\n", f"require('{paths.get_bc_path().replace('\\', '\\\\')}');"])
+        except Exception as e:
+            print(e)
--- paths/windows_paths.py
+++ paths/windows_paths.py
@@ -2,41 +2,47 @@ import os
 import re
 import requests
 import tempfile
-
+from abstract.abstract_paths import AbstractPaths
 #   The code checks if the discord path contains a specific file, which better discord injects itself into,
 #   if the file exists, we will return is path, otherwise we will return None.
 
 
-def get_discord_path() -> str | None:
-    sep_1 = os.getenv('LOCALAPPDATA')
+class WindowsPaths(AbstractPaths):
+    def get_discord_path(self) -> str | None:
+        sep_1 = os.getenv('LOCALAPPDATA')
 
-    if sep_1 is None:
-        return None
+        if sep_1 is None:
+            return None
 
-    discord_path = os.path.join(sep_1, 'Discord')
+        discord_path = os.path.join(sep_1, 'Discord')
 
-    if not os.path.exists(discord_path):
-        return None
+        if not os.path.exists(discord_path):
+            return None
 
-    regex_find_app_directory = re.compile(r'app-\d+\.\d+(\.\d+)?')
+        regex_find_app_directory = re.compile(r'app-\d+\.\d+(\.\d+)?')
 
-    dirs = os.listdir(discord_path)
+        dirs = os.listdir(discord_path)
 
-    app_folder = list(filter(regex_find_app_directory.match, dirs))
+        app_folder = list(filter(regex_find_app_directory.match, dirs))
 
-    if len(app_folder) != 1:
-        return None
+        if len(app_folder) != 1:
+            return None
 
-    app_folder = app_folder[0]
+        app_folder = app_folder[0]
 
-    discord_path = os.path.join(discord_path, app_folder, 'modules',
-                                'discord_desktop_core-1', 'discord_desktop_core', 'index.js')
+        discord_path = os.path.join(discord_path, app_folder, 'modules',
+                                    'discord_desktop_core-1', 'discord_desktop_core', 'index.js')
 
-    if (not os.path.exists(discord_path)):
-        return None
+        if (not os.path.exists(discord_path)):
+            return None
 
-    return discord_path
+        return discord_path
 
-def get_bc_location() -> str | None:
-    pass
+    def get_bc_path(self) -> str | None:
+        env = os.getenv('APPDATA')
 
+        if env == None:
+            raise Exception('Windows section not containing Appdata? weird.')
+
+        # FIXME: I know its bad, but right now i'll set it up statically to see how i can work on it later.
+        return os.path.join(env, 'BetterDiscord', 'data', 'betterdiscord.asar')
